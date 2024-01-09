2e30fa39806828ee387e3cee06d09a76ee981983
karthik <muddamkarthikreddy@gmail.com>
2024-01-09 14:49:27 +0530

    remove old stuff

--- personal_portfolio_project/settings.py
+++ personal_portfolio_project/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-^y6uy(#-)-5)pe4t9=n^0%xlut0&myyc8d80ujw3exd(&_2e__'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['karthikreddy046.pythonanywhere.com']
 
 
 # Application definition
@@ -126,3 +126,10 @@ MEDIA_ROOT= os.path.join(BASE_DIR / 'media',)
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+try:
+    from .local_settings import *
+except ImportError:
+    print("LOKKS like no file ")
